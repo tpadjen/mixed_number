a9603d5868812423ab0087cb4ca35bdee96d46f3
Tim Padjen <tpadjen@gmail.com>
2015-05-13 13:03:32 -0600

    Valid and invalid input

--- lib/mixed_number.rb
+++ lib/mixed_number.rb
@@ -1,5 +1,57 @@
 require "mixed_number/version"
 
-module MixedNumber
-  # Your code goes here...
+class MixedNumber < Numeric
+	extend Forwardable
+
+	attr_accessor :value
+
+	DECIMAL_NUMBER_REGEX  = /^-?\d+(.\d+)?$/
+	RATIONAL_NUMBER_REGEX = /^-?\d+\/\d+$/
+	MIXED_NUMBER_REGEX    = /^-?\d+\s+\d+\/\d+$/
+
+	def_delegators :@value, :abs
+
+	def initialize(input=0)
+		input = input.to_s.strip
+		raise MixedNumberFormatError unless is_mixed_number(input)
+		
+		reduction_method = input =~ /^-/ ? :- : :+
+		@value = input.split.map { |r| Rational(r) }.reduce(reduction_method).to_r
+	end
+
+	def ==(other)
+		@value == other
+	end
+
+	def <=>(other)
+		@value <=> other
+	end
+
+	def +(other)
+		if other.is_a? MixedNumber
+    	MixedNumber.new(value + other.value)
+    elsif other.is_a? Numeric
+    	MixedNumber.new(value + other)
+    else
+    	if other.respond_to? :coerce
+    		a, b = other.coerce(self)
+    		a + b
+    	else
+    		raise TypeError, "#{other.class} can't be coerced into MixedNumber"
+    	end
+    end
+	end
+
+	def coerce(other)
+		[MixedNumber.new(other), self]
+	end
+
+	private
+
+		def is_mixed_number(s)
+			s =~ Regexp.union(DECIMAL_NUMBER_REGEX, RATIONAL_NUMBER_REGEX, MIXED_NUMBER_REGEX)
+		end
+
+	class MixedNumberFormatError < StandardError; end
 end
+
--- spec/mixed_number_spec.rb
+++ spec/mixed_number_spec.rb
@@ -2,6 +2,113 @@ require "mixed_number"
 
 describe MixedNumber do
   
-	
-  
+	context 'Numeric : ' do
+	  
+		it 'should be a Numeric' do
+		  expect(MixedNumber.new).to be_a(Numeric)
+		end
+
+		context 'abs' do
+		  it 'finds the absolute value' do
+		    expect(MixedNumber.new("-3 2/4").abs).to eq(3.5)
+		  end
+		end
+
+		context '<=>' do
+		  it 'compares' do
+		    expect(MixedNumber.new(  0) <=>   1).to be(-1)
+		    expect(MixedNumber.new(  1) <=> 1/2).to be( 1)
+		    expect(MixedNumber.new(1.5) <=> 1.5).to be( 0)
+		  end
+		end
+
+		context '+' do
+		  it 'adds' do
+		    expect(MixedNumber.new(0) + MixedNumber.new(1)).to eq(1)
+		    expect(MixedNumber.new(1) + Rational(1, 2)).to eq(1.5)
+		    expect(MixedNumber.new("2 2/5") + 1.5).to eq(3.9)
+		  end
+
+		  it 'fails to add to something non-coercable' do
+		    expect{MixedNumber.new(1) + " sidfisdjf "}.to raise_error(TypeError)
+		    expect{" sidfisdjf " + MixedNumber.new(1)}.to raise_error(TypeError)
+		  end
+		end
+
+	end
+
+	context 'Parsing : ' do
+
+		context 'valid input :' do
+
+			it 'defaults to zero' do
+			  expect(MixedNumber.new).to eq(0)
+			end
+		  
+			it 'a Fixnum' do
+			  expect(MixedNumber.new(  1)).to eq(  1)
+			  expect(MixedNumber.new(135)).to eq(135)
+			  expect(MixedNumber.new( -1)).to eq( -1)
+			end
+
+			it 'a Rational' do
+			  expect(MixedNumber.new(Rational( 1,  2))).to eq( 0.5)
+			  expect(MixedNumber.new(Rational( 1, -2))).to eq(-0.5)
+			  expect(MixedNumber.new(Rational(-1,  2))).to eq(-0.5)
+			  expect(MixedNumber.new(Rational(-1, -2))).to eq( 0.5)
+			end
+
+			it 'a Float' do
+			  expect(MixedNumber.new( 1.5)).to eq( 3.0/2)
+			  expect(MixedNumber.new(-1.5)).to eq(-3.0/2)
+			end
+
+			context 'Strings : ' do
+			  it 'a whole number' do
+			    expect(MixedNumber.new( "1")).to eq( 1)
+			    expect(MixedNumber.new("-1")).to eq(-1)
+			  end
+
+			  it 'a fraction' do
+			    expect(MixedNumber.new(" 1/2")).to eq( 0.5)
+			    expect(MixedNumber.new("-1/2")).to eq(-0.5)
+			  end
+
+			  it 'a mixed number' do
+			    expect(MixedNumber.new(" 1 2/3")).to eq( 5.0/3)
+			    expect(MixedNumber.new("-1 2/3")).to eq(-5.0/3)
+			  end
+
+			  it 'trims input' do
+			    expect(MixedNumber.new( " 1 2/3\t\t")).to eq( 5.0/3)
+			    expect(MixedNumber.new(" -1 2/3\t\t")).to eq(-5.0/3)
+			  end
+
+			  it 'ignores extra space between whole and fractional part' do
+			  	expect(MixedNumber.new( "1 \t2/3")).to eq( 5.0/3)
+			  	expect(MixedNumber.new("-1 \t2/3")).to eq(-5.0/3)
+			  end
+
+			end
+
+		end
+
+		context 'invalid input : ' do
+		  
+			it 'raises an error when dividing by zero' do
+			  expect{MixedNumber.new("1 2/0")}.to raise_error(ZeroDivisionError)
+			  expect{MixedNumber.new(  "2/0")}.to raise_error(ZeroDivisionError)
+			end
+
+			it 'raises an error when not formatted like a mixed number' do
+			  ["", "1 1 2/3", "1/2 1", "a", "a/b", "a 3/4", "1 2 / 3", "1 -2/3", "1 2/-3", "word"].each do |mixed|
+			  	expect{MixedNumber.new(mixed)}.to raise_error(MixedNumber::MixedNumberFormatError)
+			  end
+			end
+
+		end
+
+	  
+	end
+
 end
